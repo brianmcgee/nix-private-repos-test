e991d5a012b77c22b7402e874e578372c4c233e5
Brian McGee <brian@bmcgee.ie>
2024-06-21 14:07:16 +0100

    feat: be explicit about builtins

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 rec {
   inputs = {
-    git_ssh = fetchGit {
+    git_ssh = builtins.fetchGit {
       url = "git@github.com:brianmcgee/secret.git";
       ref = "main";
     };
-    git_http = fetchGit {
+    git_http = builtins.fetchGit {
       url = "https://github.com/brianmcgee/secret.git";
       ref = "main";
     };
-    tarball = fetchTarball "https://github.com/brianmcgee/secret/archive/main.tar.gz";
-    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+    tarball = builtins.fetchTarball "https://github.com/brianmcgee/secret/archive/main.tar.gz";
+    nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
   };
 
   tests =
